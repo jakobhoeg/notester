43879ee40d17121ddfe6aecd6d5e79efb7808e48
jakobhoeg <jakobhoegm@gmail.com>
2025-10-25 13:31:57 +0200

    change sidebar hotkey to 'e'
    Prevents changing text to bold and triggering sidebar

--- components/providers/sidebar.tsx
+++ components/providers/sidebar.tsx
@@ -30,7 +30,7 @@ const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 const SIDEBAR_WIDTH = "18rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
 const SIDEBAR_WIDTH_ICON = "3.25rem";
-const SIDEBAR_KEYBOARD_SHORTCUT = "b";
+const SIDEBAR_KEYBOARD_SHORTCUT = "e";
 
 type SidebarContextProps = {
   state: "expanded" | "collapsed";
